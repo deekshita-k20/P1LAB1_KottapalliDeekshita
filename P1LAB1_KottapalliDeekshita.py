3fb4d1b845b1c1240ad908632e71dbbfc14163dc
kottapad0108 <kottapad0108@ad.faytechcc.edu>
2025-09-21 15:24:17 -0400

    Readability improvement

--- P1LAB1_KottapalliDeekshita.py
+++ P1LAB1_KottapalliDeekshita.py
@@ -1,12 +1,12 @@
-#Deekshita
-#09/21/2025
-#P1LAB1
-#this program asks the user for their first and last name
-#and displays a personalized welcome message. 
+# Deekshita
+# 09/21/2025
+# P1LAB1
+# This program asks the user for their first and last name
+# and displays a personalized welcome message. 
 
-#ask user for first and last name 
+# Ask user for first and last name 
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
-#display the welcome message
+# Display the welcome message
 print("Hello,", first_name, last_name + "!", "Welcome to CTI-110.") 
